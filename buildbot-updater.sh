c84767f4a8f90399ad1624e78af2e340d1d4d6ef
tannisroot <10602045+tannisroot@users.noreply.github.com>
2022-02-14 07:15:33 +0300

    Upgrade, not update

--- buildbot-updater.sh
+++ buildbot-updater.sh
@@ -2,7 +2,7 @@
 set -e
 vagrant up
 vagrant ssh -c "ssh ubuntu@buildbot-bionic-amd64 \"sudo apt update\""
-vagrant ssh -c "ssh ubuntu@buildbot-bionic-i386 \"sudo apt update\""
+vagrant ssh -c "ssh ubuntu@buildbot-bionic-i386 \"sudo apt upgrade\""
 vagrant ssh -c "ssh ubuntu@buildbot-bionic-amd64 \"cd buildbot && git fetch origin && git reset --hard origin/master\""
 vagrant ssh -c "ssh ubuntu@buildbot-bionic-i386 \"cd buildbot && git fetch origin && git reset --hard origin/master\""
 vagrant halt
